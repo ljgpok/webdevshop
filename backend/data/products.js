b23210f27332a395620b50a24a593d9f66413cd5
Lal Gurung <ljgpok@yahoo.com>
2021-01-14 01:08:55 +0200

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,102 @@
+const products = [
+  {
+    name: 'The Code Is Broke And Full Of Errors',
+    image: '/images/brokecode.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'You are the CSS to my HTML',
+    image: '/images/htmlcss.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'SENIOR WEB DEVELOPER',
+    image: '/images/seniordev.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Be nice to me. You may need tech support ..',
+    image: '/images/techsupport.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'head and body tags - Web Dev humor ',
+    image: '/images/headbody.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Geek Tee - CSS Jokes - Ninja',
+    image: '/images/ninja.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+
+  {
+    name: 'Straight Outta Coding T-shirt',
+    image: '/images/outtacoding.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  },
+
+  {
+    name: 'I Turn Coffee Into Programming Code',
+    image: '/images/coffeecode.jpg',
+    description:
+      'T-shirt',
+    brand: 'WebDev',
+    category: 'Clothes',
+    price: 20,
+    countInStock: 100,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
